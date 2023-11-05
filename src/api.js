5191ae684396cf39783c10018c9cee463eb8b441
Zhoomart Akimov <joomart.online@gmail.com>
2023-11-06 02:23:14 +0600

    remote api address setup

--- src/api.js
+++ src/api.js
@@ -5,8 +5,8 @@ import Cookies from 'js-cookie';
 export const getAccessToken = () => Cookies.get('accessToken');
 
 const instance = axios.create({
-  // baseURL: 'https://api.primeholding.kg/v1',
-  baseURL: 'http://localhost:8080/v1',
+  baseURL: 'https://api.onmywaynow.ru/v1',
+  // baseURL: 'http://localhost:8080/v1',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
